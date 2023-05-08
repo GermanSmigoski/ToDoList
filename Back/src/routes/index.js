c1538a74899b847f982f3b4f825f2a42e0dfe397
German <haspenngames14@gmail.com>
2023-05-07 22:34:12 -0300

    to do list swagger

--- Back/src/routes/index.js
+++ Back/src/routes/index.js
@@ -2,13 +2,11 @@ const { Router } = require("express");
 const express = require("express");
 const list = require("./list/list");
 const user = require("./user/user");
-const info = require("./info/info");
 
 const router = Router();
 
 router.use(express.json());
 router.use("/list", list);
 router.use("/user", user);
-router.use("/info", info);
 
 module.exports = router;
--- Back/src/routes/info/info.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require("express");
-const { loadInfo } = require("../../controllers/infoController");
-
-const router = Router();
-
-router.post("/", loadInfo);
-
-module.exports = router;
